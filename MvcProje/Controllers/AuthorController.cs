560f4c5acee7e112ec202095a9d80208373dddda
Mustafa ali kağan küçük <65673292+malikagan48@users.noreply.github.com>
2023-09-27 21:02:55 +0300

    update

---
+++ MvcProje/Controllers/AuthorController.cs
@@ -0,0 +1,142 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace MvcProje.Controllers
+{
+    public class AuthorController : Controller
+    {
+        // GET: Author
+        private Context db = new Context();
+
+        BlogManager blogmanager = new BlogManager(new EfBlogDal());
+        AuthorManager authormanager = new AuthorManager(new EfAuthorDal());
+
+        [AllowAnonymous]
+        public PartialViewResult AuthorAbout(int id)
+        {
+            var authordetail = blogmanager.GetBlogByID(id);
+            return PartialView(authordetail);
+        }
+        [AllowAnonymous]
+        public PartialViewResult AuthorPopularPost(int id)
+        {
+            var blogauthorid = blogmanager.GetList().Where(x => x.BlogID == id).Select(y => y.AuthorID).FirstOrDefault();
+
+            var authorblogs = blogmanager.GetBlogByAuthor(blogauthorid);
+            return PartialView(authorblogs);
+        }
+
+
+        public ActionResult AuthorList()
+        {
+            var authorlists = authormanager.GetList();
+            return View(authorlists);
+        }
+
+        [HttpGet]
+        public ActionResult AddAuthor()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult AddAuthor([Bind(Include = "AuthorID,AuthorName,AuthorImage,AuthorAbout,AuthorTitle,AboutShort,Mail,Password,PhoneNumber,Blogs")]Author t, HttpPostedFileBase AuthorImage)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                if (AuthorImage != null)
+                {
+                    WebImage img = new WebImage(AuthorImage.InputStream);
+                    FileInfo imginfo = new FileInfo(AuthorImage.FileName);
+
+                    string logoname = Guid.NewGuid().ToString() + imginfo.Extension;
+                    img.Resize(500, 500);
+                    img.Save("~/Uploads/AuthorImage/" + logoname);
+
+                    t.AuthorImage = "/Uploads/AuthorImage/" + logoname;
+                }
+                db.Authors.Add(t);
+                db.SaveChanges();
+                return RedirectToAction("AuthorList");
+            }
+            AuthorValidator authorValidator = new AuthorValidator();
+            ValidationResult results = authorValidator.Validate(t);
+            if (results.IsValid)
+            {
+                authormanager.TAdd(t);
+                return RedirectToAction("AuthorList");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+
+            return View(t);           
+        }
+        [HttpGet]
+        public ActionResult AuthorEdit(int id)
+        {
+            Author author = authormanager.GetByID(id);
+            return View(author);
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult AuthorEdit([Bind(Include = "AuthorID,AuthorName,AuthorImage,AuthorAbout,AuthorTitle,AboutShort,Mail,Password,PhoneNumber,Blogs")]int? id, Author t, HttpPostedFileBase AuthorImage)
+        {
+         
+            if (ModelState.IsValid)
+            {
+               
+                var h = db.Authors.Where(x => x.AuthorID == id).SingleOrDefault();
+                if (AuthorImage != null)
+                {
+                
+                    WebImage img = new WebImage(AuthorImage.InputStream);
+                    FileInfo imginfo = new FileInfo(AuthorImage.FileName);
+
+                    string hizmetname = Guid.NewGuid().ToString() + imginfo.Extension;
+                    img.Resize(500, 500);
+                    img.Save("~/Uploads/AuthorImage/" + hizmetname);
+
+                    h.AuthorImage = "/Uploads/AuthorImage/" + hizmetname;
+                }
+                authormanager.TUpdate(t);
+
+                db.SaveChanges();
+                return RedirectToAction("AuthorList");
+            }
+            return View(t);
+        }
+        public ActionResult Delete(int id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            var h = db.Authors.Find(id);
+            if (h == null)
+            {
+                return HttpNotFound();
+            }
+            db.Authors.Remove(h);
+            db.SaveChanges();
+            return RedirectToAction("AuthorList");
+
+        }
+    }
+}
---
+++ MvcProje/Controllers/UserController.cs
@@ -0,0 +1,175 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using PagedList;
+using PagedList.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MvcProje.Controllers
+{
+    [Authorize]
+    public class UserController : Controller
+    {
+        // GET: User
+        private Context db = new Context();
+
+        UserProfileManager userProfile = new UserProfileManager();
+        BlogManager bm = new BlogManager(new EfBlogDal());
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult Partial1(string p)
+        {
+            p = (string)Session["Mail"];
+            var profilevalues = userProfile.GetAuthorByMail(p);
+            return PartialView(profilevalues);
+        }
+        public ActionResult UpdateUserProfile(Author p)
+        {
+            userProfile.EditAuthor(p);
+            return RedirectToAction("Index");
+        }
+        public ActionResult BlogList(string p)
+        {
+            p = (string)Session["Mail"];
+            Context c = new Context();
+            int id = c.Authors.Where(x => x.Mail == p).Select(y => y.AuthorID).FirstOrDefault();
+            var blogs = userProfile.GetBlogByAuthor(id);
+            return View(blogs);
+        }
+        [HttpGet]
+        public ActionResult UpdateBlog(int id)
+        {
+            Blog blog = bm.GetByID(id);
+            Context c = new Context();
+            List<SelectListItem> values = (from x in c.Categories.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CategoryName,
+                                               Value = x.CategoryID.ToString()
+                                           }).ToList();
+            ViewBag.values = values;
+
+            List<SelectListItem> values2 = (from x in c.Authors.ToList()
+                                            select new SelectListItem
+                                            {
+                                                Text = x.AuthorName,
+                                                Value = x.AuthorID.ToString()
+                                            }).ToList();
+            ViewBag.values2 = values2;
+            return View(blog);
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult UpdateBlog([Bind(Include = "BlogID,BlogTitle,CategoryID,BlogImage,BlogDate,AuthorID,BlogContent")]Blog t, HttpPostedFileBase BlogImage, int? id)
+        {
+            if (ModelState.IsValid)
+            {
+                var s = db.Blogs.Where(x => x.BlogID == id).SingleOrDefault();
+                if (BlogImage != null)
+                {
+                    WebImage img = new WebImage(BlogImage.InputStream);
+                    FileInfo imginfo = new FileInfo(BlogImage.FileName);
+
+                    string logoname = Guid.NewGuid().ToString() + imginfo.Extension;
+                    img.Resize(500, 500);
+                    img.Save("~/Uploads/BlogImage/" + logoname);
+
+                    t.BlogImage = "/Uploads/BlogImage/" + logoname;
+                }
+                bm.TUpdate(t);
+      
+                db.SaveChanges();
+                return RedirectToAction("BlogList");
+            }
+            return View(t);
+        }
+        [HttpGet]
+        public ActionResult AddNewBlog()
+        {
+            Context c = new Context();
+            List<SelectListItem> values = (from x in c.Categories.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CategoryName,
+                                               Value = x.CategoryID.ToString()
+                                           }).ToList();
+            ViewBag.values = values;
+
+            List<SelectListItem> values2 = (from x in c.Authors.ToList()
+                                            select new SelectListItem
+                                            {
+                                                Text = x.AuthorName,
+                                                Value = x.AuthorID.ToString()
+                                            }).ToList();
+            ViewBag.values2 = values2;
+            return View();
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult AddNewBlog([Bind(Include = "BlogID,BlogTitle,CategoryID,BlogImage,BlogDate,AuthorID,BlogContent")]Blog t, HttpPostedFileBase BlogImage)
+        {
+            if (ModelState.IsValid)
+            {
+                if (BlogImage != null)
+                {
+                    WebImage img = new WebImage(BlogImage.InputStream);
+                    FileInfo imginfo = new FileInfo(BlogImage.FileName);
+
+                    string logoname = Guid.NewGuid().ToString() + imginfo.Extension;
+                    img.Resize(500, 500);
+                    img.Save("~/Uploads/BlogImage/" + logoname);
+
+                    t.BlogImage = "/Uploads/BlogImage/" + logoname;
+                }
+                db.Blogs.Add(t);
+                db.SaveChanges();
+                return RedirectToAction("BlogList");
+            }
+            BlogValidator blogValidator = new BlogValidator();
+            ValidationResult results = blogValidator.Validate(t);
+            if (results.IsValid)
+            {
+                bm.TAdd(t);
+                
+                return RedirectToAction("BlogList");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+
+            return View(t);
+        }
+
+        public ActionResult DeleteBlog(int id)
+        {
+            Blog blog = bm.GetByID(id);
+            bm.TDelete(blog);
+            return RedirectToAction("BlogList");
+        }
+
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            Session.Abandon();
+            return RedirectToAction("AuthorLogin", "Login");
+        }
+
+    }
+}
